8ac8d36c6d32b825b0250667758aa1d19cae8b7c
anderson-robertp <Anderon-RP@hotmail.com>
2024-07-25 15:46:14 -0500

    route

--- dinnerspinnerV2/vite.config.js
+++ dinnerspinnerV2/vite.config.js
@@ -6,5 +6,19 @@ export default defineConfig({
   plugins: [svelte()],
   define: {
     'process.env': process.env
-}
-})
+  },
+  root: "src/",
+
+  build: {
+    outDir: "../dist",
+    rollupOptions: {
+      input: {
+        main: resolve(__dirname, "src/index.html"),
+        translator: resolve(__dirname, "src/translator/index.html"),
+        preferences: resolve(__dirname, "src/preferences/index.html"),
+        register: resolve(__dirname, "src/register/index.html"),
+        login: resolve(__dirname, "src/login/index.html"),
+      },
+    }
+  }
+});
